b265ce31a7da0130d190dbb0641e683ad33c9b5c
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2025-10-10 10:05:33 -0400

    Update outputs.tf

--- terraform-modules/terraform-aws-s3-bucket/outputs.tf
+++ terraform-modules/terraform-aws-s3-bucket/outputs.tf
@@ -3,7 +3,12 @@ output "bucket_name" {
   value       = aws_s3_bucket.this.id
 }
 
-output "arn" {
+output "bucket_id" {
+  description = "The name (ID) of the S3 bucket"
+  value       = aws_s3_bucket.this.id
+}
+
+output "bucket_arn" {
   description = "The ARN of the S3 bucket"
   value       = aws_s3_bucket.this.arn
 }
