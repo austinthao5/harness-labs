7b7043fb72b55d0619c014cc46783d5b9600e630
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2026-06-02 15:49:50 -0400

    Create variables.tf

---
+++ terraform-output-only/variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  type        = string
+  description = "Deployment environment"
+}
+
+variable "application" {
+  type        = string
+  description = "Application name"
+}
